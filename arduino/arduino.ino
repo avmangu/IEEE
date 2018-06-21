118a004d3ceaac86e05c1c083ca589396f811ffa
Animesh Mangu <you@example.com>
2018-06-20 17:30:38 -0700

    Labs

---
+++ arduino/arduino.ino
@@ -0,0 +1,142 @@
+#include <printf.h>
+#include <RF24.h>
+#include <nrf24L01.h>
+
+#define RED_BUT 9 
+#define YEL_BUT 8
+#define GRN_BUT 7
+
+//having an array of 3 values helps with debouncing
+//'memory aspect'
+bool rbutt[3] = {false, false, false};
+bool ybutt[3] = {false, false, false}; 
+bool gbutt[3] = {false, false, false};
+
+// maximum limit for buffers. 
+#define BUFFER_SIZE 28  //we chose this such that it would be less than 32 with the extra one
+
+//buf is the buffer that we build on the arduino side
+char buf[2 * BUFFER_SIZE];
+
+//correct is the buffer that we compare it to
+char correct[2 * BUFFER_SIZE];
+
+RF24 controller(14, 15); //we declare a RF24 controller
+
+//this is the current number of buttons saved on the arduino
+uint8_t cur_buf_len = 0;
+
+uint8_t turn = 0;
+
+//this is the current turn
+//turn + 1 is going to be the wanted length of the buffer
+
+typedef struct {
+  uint8_t b[BUFFER_SIZE];
+  uint8_t turn=0;
+} Data;
+
+Data d;
+
+//this is the struct that we send back to acknowledge if it is correct or not
+typedef struct { 
+  uint8_t turn_correct;
+} Ack;
+
+Ack a; 
+
+void radio_init() {
+  controller.begin();
+  
+  controller.setPALevel(RF24_PA_MIN);
+  controller.setPayloadSize(32);
+  controller.setChannel(7);
+  controller.setCRCLength(RF24_CRC_16);
+  controller.setDataRate(RF24_1MBPS);
+  controller.openReadingPipe(1, 0xDC);
+  controller.openWritingPipe(0xCC);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  radio_init();
+  printf_begin();
+
+  //buttons are inputs
+  pinMode(RED_BUT, INPUT); 
+  pinMode(YEL_BUT, INPUT);
+  pinMode(GRN_BUT, INPUT);
+
+  Serial.begin(9600);
+  controller.printDetails(); //prints details before starting
+}
+
+void updatevalues(int button, bool arr[3],  char letter){  //check for button presses
+  //constantly keeps the last three read button values
+  arr[0] = arr[1];
+  arr[1] = arr[2];
+  arr[2] = (bool) digitalRead(button);
+  
+  //basically if it detects that it was low, and then becomes high
+  //transmission is successfull
+  if ( !arr[0] && arr[1] && arr[2] ) {
+    buf[cur_buf_len] = letter; //adds the letter to the array
+    cur_buf_len++; //re-sets end of buffer. 
+    Serial.write(letter);
+    Serial.println();
+  }
+
+}
+
+// returns 0 if the buttons pressed differ from the received buf.
+// else returns 1. 
+int compare()
+{
+  for (int i=0; i < turn; i++)
+  {
+    if (correct[i] != buf[i])
+      return 0;
+  }
+  return 1;
+}
+
+void loop() {
+  // update the button arrays with the current button status.
+  controller.startListening();
+  while(!controller.available()); //do nothing if there is nothing to read
+  controller.read(&d, sizeof(d));  //reads the first half of data into variable d
+  d = (Data)d;
+  for (int i = 0; i < BUFFER_SIZE; i++)
+    correct[i] = d.b[i]; //writes this into the correct array
+  turn = d.turn;
+
+Serial.println("First half of message received.");
+
+  while(!controller.available()); //do nothing if there is nothing to read
+  controller.read(&d, sizeof(d)); //reads the second half of data into variable d
+  d = (Data)d;
+  for (int i = 0; i < BUFFER_SIZE; i++)
+    correct[i + BUFFER_SIZE] = d.b[i]; //writes this into the second half of the array
+  if ( turn != d.turn )
+    Serial.println("something messed up with synchro.");
+
+  controller.stopListening();
+
+  Serial.println("Entire message received.");
+
+  cur_buf_len = 0; // reset the buffer just after receiving. 
+
+  while (cur_buf_len < turn) { //check for button presses
+    updatevalues(RED_BUT, rbutt, 'R'); 
+    updatevalues(YEL_BUT, ybutt, 'Y');
+    updatevalues(GRN_BUT, gbutt, 'G');
+  }
+
+  uint8_t cmp_result = compare();
+  a.turn_correct = cmp_result;
+  while(!controller.write(&a, sizeof(a)));
+  if ( cmp_result )
+    Serial.println("You did it!");
+  else
+    Serial.println("Unlucky.");
+}
---
+++ teensy/teensy.ino
@@ -0,0 +1,127 @@
+#include <RF24.h>
+#include <printf.h>
+
+#define BUFFER_LENGTH 60
+uint8_t buf[BUFFER_LENGTH];
+
+#define RED_LED 17
+#define YELLOW_LED 18
+#define GREEN_LED 15
+
+uint32_t turn = 1;
+
+typedef struct {
+  uint8_t buf[30];
+  uint8_t turn = 1;
+} Data;
+
+typedef struct {
+  uint8_t correct;
+} Check;
+
+Data data;
+Check checker;
+
+RF24 radio(9, 10);
+
+void init() {
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(32);
+  radio.setChannel(15);
+  radio.setCRCLength(RF24_CRC_16);
+  radio.setDataRate(RF24_1MBPS);
+  radio.openReadingPipe(1, 0xc2c2c2c2c2);
+  radio.openWritingPipe(0xe7e7e7e7e7);
+}
+
+void setup() {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+  Serial.begin(9600);
+
+  radio.begin();
+  init();
+ 
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+}
+
+uint8_t rngColor()
+{
+  int value = random(3);
+  switch(value) {
+    case 0:
+      return 'R';
+    case 1:
+      return 'G';
+    case 2:
+      return 'Y';
+  }
+}
+
+void setLED(uint8_t color, uint8_t status)
+{
+  switch(color) {
+    case 'R': 
+      digitalWrite(RED_LED, status);
+      break;
+    case 'G':
+      digitalWrite(GREEN_LED, status); 
+      break;
+    case 'Y': 
+      digitalWrite(YELLOW_LED, status);
+      break;
+  }
+}
+
+void loop() 
+{
+  buf[turn - 1] = rngColor();
+
+  for (uint32_t i = 0; i < turn; i++) {
+    setLED(buf[i], HIGH);   
+    delay(1000);
+    setLED(buf[i], LOW);
+    delay(300);
+  }
+  data.turn = turn;
+
+  for (int i = 0; i < 30; i++) {
+    data.buf[i] = buf[i];
+  }
+  
+  for ( int i = 0; i < 30; i++ ) {
+    data.buf[i] = buf[30 + i];
+  }
+  
+  radio.startListening();
+  while (!radio.available()) {}
+  
+  radio.read(&checker, sizeof(checker));
+  uint8_t input = checker.correct;
+  radio.stopListening();
+  
+  switch(input) {
+    case 0: 
+      turn = 1;
+      digitalWrite(RED_LED, HIGH);
+      delay(1000);
+      digitalWrite(RED_LED, LOW);
+      delay(1000);
+      Serial.println("Game Over.");
+      break;
+    case 1:  
+      turn++;
+      digitalWrite(GREEN_LED, HIGH);
+      delay(1000);
+      digitalWrite(GREEN_LED, LOW);
+      delay(1000);
+      Serial.println("Correct Code.");
+      break;
+    default: 
+      break;
+  }
+}
